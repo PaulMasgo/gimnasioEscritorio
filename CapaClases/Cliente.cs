4cae025c1abd1ae559bce2049952b5f1c1703515
JeanRamos <i1415905@continental.edu.pe>
2019-07-30 14:28:37 -0500

    segunda etapa

---
+++ CapaClases/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapaClases
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string dni { get; set; }
+        public string telefono { get; set; }
+        public string telefonoEmergencia { get; set; }
+        public DateTime nacimiento { get; set; }
+
+
+    }
+}
